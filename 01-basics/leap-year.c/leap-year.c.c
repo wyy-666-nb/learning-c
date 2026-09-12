f99c2a3b6ebe69129d0ff32150f1f3183235693b
WangYaoyang <1423219735@qq.com>
2026-09-12 18:18:44 +0800

    feat: 添加闰年判断

---
+++ 01-basics/leap-year.c/leap-year.c.c
@@ -0,0 +1,24 @@
+//判断闰年小程序
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int year = 0;
+	printf("请输入年份：");
+	scanf("%d", &year);
+	if (year % 4 == 0)
+	{
+		if (year % 100 != 0)
+		{
+			printf("%d年是闰年\n", year);
+		}
+		else if (year % 400 == 0)
+		{
+			printf("%d年是闰年\n", year);
+		}
+		else
+			printf("%d年不是闰年\n", year);
+	}
+	else
+		printf("%d年不是闰年\n", year);
+}
