794b6b3295a7aae2b5d57677b30948c7b1112aea
WangYaoyang <1423219735@qq.com>
2026-09-19 14:17:34 +0800

    feat: 添加阶乘求和程序

---
+++ 01-basics/sum-of-factorials/sum-of-factorials.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int j = 0;
+//	int m = 1;
+//	int q = 0;
+//	printf("请输入n的值：");
+//	scanf("%d", &n);
+//	for (j = 1;j <= n;j++)
+//	{
+//		m = 1;
+//		for (i = 1;i <= j;i++)
+//		{
+//			m *= i;
+//		}
+//		q += m;
+//	}
+//	printf("%d", q);
+//
+//	return 0;
+//
+int main()
+{
+	int n = 0;
+	int j = 0;
+	int m = 1;
+	int q = 0;
+	printf("请输入n的值：");
+	scanf("%d", &n);
+	for (j = 1;j <= n;j++)
+	{
+		m *= j;
+		q += m;
+	}
+	printf("%d", q);
+
+	return 0;
+
+}
