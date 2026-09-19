b01a2172e3293ee54aabb6e591d773ca59144f0a
WangYaoyang <1423219735@qq.com>
2026-09-19 15:36:32 +0800

    feat: 添加星号菱形图案

---
+++ 01-basics/star-pattern/star-pattern.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int n = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	printf("您想打几行的图案：");
+	scanf("%d", &n);
+	for (i = 1;i <= n;i++)
+	{
+		for (j = 1;j <= n-i;j++)
+		{
+			printf(" ");
+		}
+		for (k = 1;k <= 2 * i - 1;k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (i = 1;i<= n - 1;i++)
+	{
+		for (j = 1;j <= i;j++)
+		{
+			printf(" ");
+		}
+		for (k = 1;k <= 2 * n - 2 * i -1;k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
