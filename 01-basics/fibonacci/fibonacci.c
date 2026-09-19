5aa358e0b10fb2482ffdd97e6d9d1a7e89a94062
WangYaoyang <1423219735@qq.com>
2026-09-19 14:31:42 +0800

    feat: 添加斐波那契数列程序

---
+++ 01-basics/fibonacci/fibonacci.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a = 1;
+	int b = 1;
+	int c = 0;
+	int i = 0;
+	for (i = 0;i <= 20;i++)
+	{
+		printf("%d  ", a);
+		c = a + b;
+	    a = b;
+		b = c;
+	}
+
+
+	return 0;
+}
