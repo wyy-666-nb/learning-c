a4f4f175d2f47d5559fff5f0d6e7ffe9f4ab5202
WangYaoyang <1423219735@qq.com>
2026-09-19 14:49:34 +0800

    feat: 添加求最大公约数程序

---
+++ 01-basics/gcd/gcd.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	printf("请输入两个数：");
+	scanf("%d  %d", &a, &b);
+	while (b!=0)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	printf("最大公约数是%d", a);
+	return 0;
+}
