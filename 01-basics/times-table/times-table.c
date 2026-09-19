4a5a5b949566c5aef8595525027e436fb15818af
WangYaoyang <1423219735@qq.com>
2026-09-19 13:44:44 +0800

    feat: 添加乘法口诀表

---
+++ 01-basics/times-table/times-table.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a = 1;
+	int b = 1;
+	int c = 0;
+	for (b = 1;b < 10;b++)
+	{
+		for (a = 1;a <= b;a++)
+		{
+			c = a * b;
+			printf("%d * %d = %-2d  ", a, b, c);
+		}
+		printf("\n");
+	}
+    return 0;
+}
